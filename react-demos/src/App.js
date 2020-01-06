0b57cfaaad0e174de9d7056ef0bac6a4edb1f19d
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-01-05 23:13:49 -0600

    test ui
    init dev ui test

--- react-demos/src/App.js
+++ react-demos/src/App.js
@@ -1,24 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Spinner from './components/test/Spinner';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+
+      <header className="header">
+        <h2><u>Demos</u></h2>
       </header>
+
+      <div className="row">
+        <div class="one-third column">
+          <Spinner />
+          <a class="button button-primary u-full-width" href="https://google.com" target="_blank" rel="noopener noreferrer">Código</a>
+        </div>
+        <div class="one-third column">
+          <Spinner />
+          <a class="button button-primary u-full-width" href="https://google.com" target="_blank" rel="noopener noreferrer">Código</a>
+        </div>
+        <div class="one-third column">
+          <Spinner />
+          <a class="button button-primary u-full-width" href="https://google.com" target="_blank" rel="noopener noreferrer">Código</a>
+        </div>
+      </div>
+
+      <div className="row">
+        <div class="one-third column">
+          <Spinner />
+          <a class="button button-primary u-full-width" href="https://google.com" target="_blank" rel="noopener noreferrer">Código</a>
+        </div>
+        <div class="one-third column">
+          <Spinner />
+          <a class="button button-primary u-full-width" href="https://google.com" target="_blank" rel="noopener noreferrer">Código</a>
+        </div>
+        <div class="one-third column">
+          <Spinner />
+          <a class="button button-primary u-full-width" href="https://google.com" target="_blank" rel="noopener noreferrer">Código</a>
+        </div>
+      </div>
+
+      <div className="row">
+        <div class="one-third column">
+          <Spinner />
+          <a class="button button-primary u-full-width" href="https://google.com" target="_blank" rel="noopener noreferrer">Código</a>
+        </div>
+        <div class="one-third column">
+          <Spinner />
+          <a class="button button-primary u-full-width" href="https://google.com" target="_blank" rel="noopener noreferrer">Código</a>
+        </div>
+        <div class="one-third column">
+          <Spinner />
+          <a class="button button-primary u-full-width" href="https://google.com" target="_blank" rel="noopener noreferrer">Código</a>
+        </div>
+      </div>             
+
+      <div className="row">
+        <div class="one-third column">
+          <Spinner />
+          <a class="button button-primary u-full-width" href="https://google.com" target="_blank" rel="noopener noreferrer">Código</a>
+        </div>
+        <div class="one-third column">
+          <Spinner />
+          <a class="button button-primary u-full-width" href="https://google.com" target="_blank" rel="noopener noreferrer">Código</a>
+        </div>
+        <div class="one-third column">
+          <Spinner />
+          <a class="button button-primary u-full-width" href="https://google.com" target="_blank" rel="noopener noreferrer">Código</a>
+        </div>
+      </div> 
+
     </div>
   );
 }
